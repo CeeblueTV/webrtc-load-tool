3419945bb1f0876dffd7f13e27c4c5db2544a8fe
Joe Turki <git@joeturki.com>
2025-03-13 00:48:05 +0200

    chore: init project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CeeblueTV/webrtc-load-tool
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,8 @@
+// WebRTC-load-tool is a tool for load testing WebRTC servers.
+// By using this tool, you can simulate a large number of clients connecting to a WebRTC server.
+// via WHIP signaling.
+package main
+
+func main() {
+	panic("Not implemented")
+}
